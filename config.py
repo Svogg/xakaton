003e1c7295e233944bfd453a35d025184a6c1f78
svogg <svog_nixromov@bk.ru>
2023-05-19 04:22:10 +0300

    Добавил сущности связанные с city

---
+++ config.py
@@ -0,0 +1,19 @@
+import os
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv('.dbenv'))
+
+DB_DRIVER = os.environ.get('DB_DRIVER')
+DB_CONNECTOR = os.environ.get('DB_CONNECTOR')
+DB_USER = os.environ.get('DB_USER')
+DB_PASS = os.environ.get('DB_PASS')
+DB_HOST = os.environ.get('DB_HOST')
+DB_PORT = os.environ.get('DB_PORT')
+DB_NAME = os.environ.get('DB_NAME')
+
+
+'''
+insert into public.alias (id, alias_name) values (2, 'RRR')
+select * from public.city
+select * from public.alias
+'''
